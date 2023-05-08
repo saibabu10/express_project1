175d3e5ace0b12f12dd8dfc580157674683bb11b
sai babu <saibabu@sais-MacBook-Air-2.local>
2023-05-08 11:21:45 +0530

    list of services added in service page

--- routes/services.js
+++ routes/services.js
@@ -1,6 +1,27 @@
 const express = require("express")
 const service = express.Router()
+const listOfServices=[
+    {
+    'id':1,
+    'sname':'computer hardware'
+},{
+    'id':2,
+    'sname':'computer software'
+},{
+    'id':3,
+    'sname':'Network support'
+}]
 service.get('/',(request,response)=>{
-    response.send("This is service")
+    response.send("service page")
+})
+service.get('/:id',(request,response)=>{
+const serviceId = Number(request.params.id)
+const isAvail = listOfServices.find((service)=>service.id===serviceId)
+if(!isAvail){
+    response.send("invalid")
+}
+else{
+    response.json(isAvail.sname)
+}
 })
 module.exports = service
