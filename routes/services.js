addc40de7a85edf462ceb81267f3be9eb1f477b7
sai babu <saibabu@sais-MacBook-Air-2.local>
2023-05-08 11:54:58 +0530

    middleware added

--- routes/services.js
+++ routes/services.js
@@ -2,26 +2,31 @@ const express = require("express")
 const service = express.Router()
 const listOfServices=[
     {
-    'id':1,
+   
     'sname':'computer hardware'
 },{
-    'id':2,
+   
     'sname':'computer software'
 },{
-    'id':3,
+    
     'sname':'Network support'
 }]
 service.get('/',(request,response)=>{
     response.send("service page")
 })
 service.get('/:id',(request,response)=>{
-const serviceId = Number(request.params.id)
-const isAvail = listOfServices.find((service)=>service.id===serviceId)
-if(!isAvail){
-    response.send("invalid")
-}
-else{
-    response.json(isAvail.sname)
-}
+// const serviceId = Number(request.params.id)
+// const isAvail = listOfServices.find((service)=>service.id===serviceId)
+// if(!isAvail){
+//     response.send("invalid")
+// }
+// else{
+//     response.json(isAvail.sname)
+// }
+response.send(request.serviceuser.sname)
+})
+service.param('id',(request,response,next,id)=>{
+    request.serviceuser = listOfServices[id-1]
+    next()
 })
 module.exports = service
